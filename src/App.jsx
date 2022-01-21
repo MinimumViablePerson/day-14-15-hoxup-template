34f5926d2e0d2294f913b8892dd5e4bdbc5ae91b
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-01-21 10:47:17 +0000

    Final review

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
+import Modal from './components/Modal'
 import Login from './pages/Login'
 import Main from './pages/Main'
 import NotFound from './pages/NotFound'
@@ -39,33 +40,31 @@ export default function App () {
         <Route
           path='/logged-in'
           element={
-            <Main currentUser={currentUser} logOut={logOut} users={users} />
+            <Main
+              currentUser={currentUser}
+              logOut={logOut}
+              users={users}
+              setModal={setModal}
+              modal={modal}
+            />
           }
         />
         <Route
           path='/logged-in/:conversationId'
           element={
-            <Main currentUser={currentUser} logOut={logOut} users={users} />
+            <Main
+              modal={modal}
+              currentUser={currentUser}
+              logOut={logOut}
+              users={users}
+              setModal={setModal}
+            />
           }
         />
         <Route path='*' element={<NotFound />} />
       </Routes>
 
-      {modal === 'new-user' ? (
-        <div className='modal-wrapper'>
-          <div className='modal'>
-            <h1>New user modal</h1>
-          </div>
-        </div>
-      ) : null}
-
-      {modal === 'something-else' ? (
-        <div className='modal-wrapper'>
-          <div className='modal'>
-            <h1>Something else</h1>
-          </div>
-        </div>
-      ) : null}
+      {/* <Modal modal={modal} setModal={setModal} /> */}
     </div>
   )
 }
